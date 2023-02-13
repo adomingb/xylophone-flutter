865aec0a0ddb746ca9b91a2c79038af039186182
adomingb <adomingb@gmail.com>
2023-02-13 04:45:57 +0100

    Final

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,40 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNum) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNum.wav'));
+  }
+
+  Expanded buildKey(int keyNum, Color keyColor) => Expanded(
+        child: TextButton(
+          style: ButtonStyle(
+            enableFeedback: false,
+            backgroundColor: MaterialStatePropertyAll<Color>(keyColor),
+          ),
+          onPressed: () {
+            playSound(keyNum);
+          },
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.load('note1.wav');
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.purple),
+            ],
           ),
         ),
       ),
